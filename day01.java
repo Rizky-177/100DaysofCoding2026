7a1159bbbadd647eff337e109cc378d7666c816a
Rizky-177 <muhrizkyalfajri2007@gmail.com>
2026-09-03 00:06:50 +0800

    Create day01.java

---
+++ day01.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Komentar satu baris
+
+        /* Komentar banyak baris */
+
+        /** Komentar dokumentasi */
+
+        System.out.println("Belajar ngoding Day 1");
+    }
+}
