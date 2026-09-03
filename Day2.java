d7b3efcda0fda9f778d53dc1825487458920b37b
Rizky-177 <muhrizkyalfajri2007@gmail.com>
2026-09-03 21:27:26 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+public class BiodataDiri {
+    public static void main(String[] args) {
+
+        // println() untuk menampilkan teks dan pindah baris
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama: Muh Rizky Alfajri");
+
+        // print() untuk menampilkan teks tanpa pindah baris
+        System.out.print("Umur: ");
+        System.out.print("18 tahun\n");
+
+        // printf() untuk menampilkan teks dengan format
+        System.out.printf("Alamat: %s%n", "Kalukku");
+        System.out.printf("Status: %s%n", "Mahasiswa");
+        System.out.printf("Jenis Kelamin: %s%n", "Laki-laki");
+    }
+}
